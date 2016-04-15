9d78428f7403758f3b86cacd295af407bbb64d12
Leandro Boscariol <leandroboscariol@gmail.com>
2016-04-15 15:43:26 -0300

    Fetching local file using $http service

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // MODULE
-var drivesWhere = angular.module('drivesWhere', ['ngRoute']);
+var app = angular.module('app', ['ngRoute']);
 
-drivesWhere.config(function ($routeProvider) {
+app.config(function ($routeProvider) {
   $routeProvider
   .when('/', {
     templateUrl: 'pages/main.html',
@@ -15,20 +15,50 @@ drivesWhere.config(function ($routeProvider) {
   });
 });
 
-// SERVICES
-drivesWhere.service('searchCountry', [function () {
-  
+// FACTORIES
+app.factory('countryFactory', ['$http', '$log', '$q', function ($http, $log) {
+  var countriesPromise = null,
+    fetchCountriesPromise,
+    getCountry;
+
+  fetchCountriesPromise = function () {
+    $log.debug('fetching promise');
+    if (!countriesPromise) {
+      $log.debug('promise doesnt exist yet');
+      countriesPromise = $http.get('countries.json').then(function (response) {
+        $log.debug('fetched file');
+        return response.data;
+      });
+    }
+    return countriesPromise;
+  };
+
+  getCountry = function (country) {
+    return fetchCountriesPromise().then(function (data) {
+      return data[country];
+    });
+  };
+
+  return {
+    getCountry: getCountry
+  };
 }]);
 
 // CONTROLLERS
-drivesWhere.controller('mainController', ['$location', function ($location) {
-  this.submit = function () {
-    if (this.country) {
-      $location.path(this.country);
+app.controller('mainController', ['$location', 'countryFactory',
+  function ($location, countryFactory) {
+    countryFactory.getCountry('a');
+    this.submit = function () {
+      if (this.country) {
+        $location.path(this.country);
+      }
     }
-  }
 }]);
 
-drivesWhere.controller('countryController', ['$routeParams', function ($routeParams) {
-  this.country = $routeParams.country || 'Brazil';
+app.controller('countryController', ['$routeParams', '$scope', '$log', 'countryFactory',
+  function ($routeParams, $scope, $log, countryFactory) {
+    $scope.country = $routeParams.country || 'Brazil';
+    countryFactory.getCountry($scope.country).then(function (data) {
+      $scope.side = data;
+    });
 }]);
